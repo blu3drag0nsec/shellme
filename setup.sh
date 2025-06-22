8d4b04c0978fe076ddc8f42f9b0b8a2a00b794c6
blu3drag0nsec@gmail.com <blu3drag0nsec@gmail.com>
2025-06-22 09:57:02 -0400

    Update tools

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo ln -s /blu3/hackz/shellme/genshellme.py /usr/bin/genshellme.py
-sudo ln -s /blu3/hackz/shellme/getmyip.sh /usr/bin/getmyip.sh
+sudo ln -s /opt/blu3/shellme/genshellme.py /usr/bin/genshellme.py
+sudo ln -s /opt/blu3/shellme/getmyip.sh /usr/bin/getmyip.sh
